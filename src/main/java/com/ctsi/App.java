7ff03664e3a8637f7918cf39e68fd96a26e36f8c
wuyufei123 <51846450+wuyufei123@users.noreply.github.com>
2020-12-28 16:58:56 +0800

    pom配置

--- src/main/java/com/ctsi/App.java
+++ src/main/java/com/ctsi/App.java
@@ -1,14 +1,20 @@
 package com.ctsi;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
-public class App 
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@MapperScan(basePackages="com.ctsi",annotationClass= Mapper.class)
+@EnableScheduling
+public class App
 {
     public static void main( String[] args )
     {
